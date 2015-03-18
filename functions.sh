70ceddf77a56b7a0c405c6cd7c9b00cb81bbbb1d
Tony F <forsythetony@gmail.com>
2015-03-18 13:03:15 -0500

    Hi

--- functions.sh
+++ functions.sh
@@ -28,3 +28,54 @@ cdl()
 	    echo The directory $target is empty '(or non-existent)'
 	fi
 }
+
+mcd()
+{
+	if [ $# -ne 1 ]; then
+		exit
+	fi
+	
+	if [[ ! "$1" =~ [^0-9a-z-] ]] ; then  
+	    echo "Valid String"; 
+	else 
+		echo "String not valid";
+		exit
+	fi
+	
+	mkdir -p "$1"
+	
+	if [ $? -ne 0 ] ; then
+	    echo "Directory not created.";
+	    exit
+	else
+	
+		echo "Directory created";
+		cd "$1";
+		pwd;
+		ll;
+	fi
+}
+
+up()
+{
+	cd ..
+	ll;
+}
+
+
+oi()
+{
+	if [ $# -ne 1 ]; then
+		exit
+	fi
+	
+	if [[ "$1" =~ ^".*"$ ]]; then
+		git commit -am "$1"
+	else
+		exit
+	fi
+	
+	
+
+
+}
